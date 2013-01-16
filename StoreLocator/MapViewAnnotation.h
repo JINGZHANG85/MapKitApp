7b55a8b211b28c2190794817f78e361733affd97
JingZhang <jingzhang85@gmail.com>
2013-01-16 20:45:35 +0000

    first commit"

---
+++ StoreLocator/MapViewAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  MapViewAnnotation.h
+//  StoreLocator
+//
+//  Created by Jing Zhang on 16/01/2013.
+//  Copyright (c) 2013 com.jingzhang85. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapViewAnnotation : NSObject
+
+@end
---
+++ StoreLocator/MapViewAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  MapViewAnnotation.m
+//  StoreLocator
+//
+//  Created by Jing Zhang on 16/01/2013.
+//  Copyright (c) 2013 com.jingzhang85. All rights reserved.
+//
+
+#import "MapViewAnnotation.h"
+
+@implementation MapViewAnnotation
+
+@end
